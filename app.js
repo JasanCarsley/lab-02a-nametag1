4abefa23253f90802cf9a7811161d49ad3322fbc
JasanCarsley <jjcars@comcast.net>
2020-09-03 22:03:37 -0700

    added button and user input

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
-const name = document.getElementById("Name");
-const nameInput = document.getElementById("Name-input");
-const nameButton = document.getElementById("Name-button");
+const name = document.getElementById('Name');
+const nameInput = document.getElementById('Name-input');
+const nameButton = document.getElementById('Name-button');
+
+nameButton.addEventListener('click', () => {
+   
+    const userInput = name.value;
+    myName.textContent = userInput;
+});
 
 
 
